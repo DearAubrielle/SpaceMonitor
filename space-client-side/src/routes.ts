206a8dd2f761757c94e9f5258840853122d431c3
boontida1 <151724174+boontida1@users.noreply.github.com>
2025-05-15 01:42:04 +0700

    Update routes.ts

--- space-client-side/src/routes.ts
+++ space-client-side/src/routes.ts
@@ -4,46 +4,19 @@ import Home from './pages/Home.tsx';
 import Login from './pages/Login.tsx';
 import Googogaga from './Googogaga.tsx';
 import Watage from './pages/Watage.tsx';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
 import FloorPlan from './pages/FloorPlan.tsx';
 import FloorplanPage from './pages/FloorplanPage.tsx';
->>>>>>> Stashed changes
-=======
-import FloorPlan from './pages/FloorPlan.tsx';
-import FloorplanPage from './pages/FloorplanPage.tsx';
->>>>>>> Stashed changes
-
 const routes = createBrowserRouter([
   {
     
     Component: App,
     children: [
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        { 
-          index: true, Component: Home },
-          {
-            path: "/googogaga",
-            Component: Googogaga,
-          },
-          { path: "/watage",
-          Component: Watage,
-        },
-=======
-=======
->>>>>>> Stashed changes
         {index: true, Component: FloorplanPage },
         {path: "/home", Component: Home },
         {path: "/googogaga", Component: Googogaga },
         {path: "/watage", Component: Watage },
         {path: "/floorplan",Component: FloorPlan},
-        {path: "/floorplanP", Component: FloorplanPage}, 
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+        {path: "/floorplanP", Component: FloorplanPage},
       ],
     },
     {path: "/login",
