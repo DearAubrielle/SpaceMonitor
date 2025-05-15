93b48f3b2477695387596d45a18b2d50230e86bc
boontida1 <151724174+boontida1@users.noreply.github.com>
2025-05-15 16:59:18 +0700

    fix

---
+++ space-client-side/src/pages/FloorPlan.tsx
@@ -0,0 +1,28 @@
+import ImageUpload from "@/components/floorplan/ImageUpload";
+import Users from "./Users";
+
+import styles from './FloorPlan.module.css';
+import Dnd from "@/components/floorplan/Dnd";
+const FloorPlan: React.FC = () => {
+    return (
+        <div>
+            <Dnd />
+            <div className={styles.container}>
+                <h1>Floor Plan</h1>
+                <p>Upload and manage your floor plans here.</p>
+            </div>
+
+            <div>
+                <ImageUpload onImageUpload={(imageData) => console.log(imageData)} />
+                <Users />
+            </div>
+            
+            
+
+        </div>
+        
+        
+    );
+};
+
+export default FloorPlan;
